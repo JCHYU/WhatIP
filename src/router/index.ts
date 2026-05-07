e62e0e0f9298295099a84879e8e702e8b70cd3ba
江崇煜 <18860116236@163.com>
2026-05-07 13:49:32 +0800

    111

---
+++ src/router/index.ts
@@ -0,0 +1,27 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import type { RouteRecordRaw } from 'vue-router'
+
+const routes: RouteRecordRaw[] = [
+  {
+    path: '/',
+    name: 'Home',
+    component: () => import('../pages/Home.vue')
+  },
+  {
+    path: '/about',
+    name: 'About',
+    component: () => import('../pages/About.vue')
+  },
+  {
+    path: '/:pathMatch(.*)*',
+    name: 'NotFound',
+    component: () => import('../pages/NotFound.vue')
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(), 
+  routes
+})
+
+export default router
